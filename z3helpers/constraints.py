135b3809cfe146e2dd6719f1724fdade485492d2
jecalles <callesjonathan@gmail.com>
2021-01-23 10:40:43 -0800

    refactors variable names for clarity

--- z3helpers/constraints.py
+++ z3helpers/constraints.py
@@ -1,40 +1,42 @@
 import itertools
 
+from synbio.codes import Code
+from synbio.utils import get_codons
 from z3 import Or
 
 from z3helpers.definitions import *
 
 
 # constrain nucleotide -> codon mapping
-def f_Codons_true_mapping():
+def f_codon_true_mapping(f_codon):
     """
-    Adds constraints on f_Codon such that triplets of nucleotides map to
+    Adds constraints on f_nuc_to_codon such that triplets of nucleotides map to
     appropriate codons
 
     :return:
     List[Bool]: constraints to be satisfied
     """
-    triples = list(itertools.product(z3nucleotides, repeat=3))
+    nucleotide_triplets = list(itertools.product(z3nucleotides, repeat=3))
     return [
-        f_Codon(n1, n2, n3) == z3codon
-        for (n1, n2, n3), z3codon in zip(triples, z3codons)
+        f_codon(n1, n2, n3) == c
+        for (n1, n2, n3), c in zip(nucleotide_triplets, z3codons)
     ]
 
 
 # hard constraints on Genetic Codes
 
 
-def at_least_one_codon_per_amino(T, exclude=(NULL)):
+def at_least_one_codon_per_amino(T, exclude=(NULL,)):
     return [
-        Or([T(z3codon) == z3amino for z3codon in z3codons])
-        for z3amino in z3aminos if z3amino not in exclude
+        Or([T(c) == aa for c in z3codons])
+        for aa in z3aminos if aa not in exclude
     ]
 
 
 def at_most_one_codon_per_amino(T, exclude=(NULL, STOP)):
-    def _any_codon_in_exclude(z3codons, T, exclude):
+    def _any_codon_in_exclude(codons, T, exclude):
         return Or([
-            Or([T(z3codon) == exc for z3codon in z3codons])
+            Or([T(c) == exc for c in codons])
             for exc in exclude
         ])
 
@@ -45,3 +47,40 @@ def at_most_one_codon_per_amino(T, exclude=(NULL, STOP)):
             # or that codons are in exclude
         ) for c1, c2 in itertools.combinations(z3codons, r=2)
     ]
+
+
+def compatible_with_standard_code(T):
+    sc = Code()
+    return [
+        Or(T(rna_to_z3codon[rna_codon]) == amino_to_z3amino[sc[rna_codon]],
+           T(rna_to_z3codon[rna_codon]) == NULL)
+        for rna_codon in triplet_rna_codons
+    ]
+
+
+# define sequence based constraints
+def translates_same(T, f_codon, seq_variables, part, offset):
+    start = part.location.start - offset
+    end = part.location.end - offset
+    codon_list = get_codons(seq_variables[start:end])
+
+    protein_seq = str(part.seq.translate())
+
+    return [
+        T(f_codon(n1, n2, n3)) == amino_to_z3amino[aa]
+        for (n1, n2, n3), aa in zip(codon_list, protein_seq)
+    ]
+
+
+# bundled constraints
+def FS20(T=f_codon_to_amino, f_codon=f_nuc_to_codon):
+    constraints = [
+        f_codon_true_mapping(f_codon),
+        at_least_one_codon_per_amino(T),
+        at_most_one_codon_per_amino(T)
+    ]
+    return list(itertools.chain(*constraints))
+
+
+def RED20(T=f_codon_to_amino, f_codon=f_nuc_to_codon):
+    return FS20(T, f_codon) + compatible_with_standard_code(T)
--- z3helpers/definitions.py
+++ z3helpers/definitions.py
@@ -1,18 +1,21 @@
 import itertools
 
+from synbio.annotations import Location
 from synbio.utils import aminoacids, dNTPs, rNTPs
-from z3 import EnumSort, Function
+from z3 import Const, EnumSort, Function
 
 __all__ = [
     # str values
     "triplet_dna_codons", "triplet_rna_codons", "aminoacids",
     # z3 Sorts and possible values
-    "N", "z3nucleotides", "C", "z3codons", "A", "z3aminos",
-    "STOP", "NULL",
+    "NucleotideSort", "z3nucleotides", "CodonSort", "z3codons",
+    "AminoSort", "z3aminos", "STOP", "NULL",
     # mappings from str to z3 values
     "dna_to_z3codon", "rna_to_z3codon", "amino_to_z3amino",
     # z3 Functions
-    "f_Code", "f_Codon"
+    "f_nuc_to_codon", "f_codon_to_amino", "f_nuc_to_amino",
+    # z3 Constant generating functions
+    "sequence_variables"
 ]
 
 # useful str collections
@@ -24,12 +27,12 @@ triplet_rna_codons = [
     ''.join(codon_list)
     for codon_list in itertools.product(rNTPs, repeat=3)
 ]
-aminoacids += ["0"]                                     # adds NULL
+aminoacids += ["0"]  # adds NULL
 
 # useful z3 Sorts
-N, z3nucleotides = EnumSort("Nucleotides", dNTPs)       # N = set of nucleotides
-C, z3codons = EnumSort("Codons", triplet_dna_codons)    # triplet codons
-A, z3aminos = EnumSort("Amino Acids", aminoacids)       # translation signals
+NucleotideSort, z3nucleotides = EnumSort("Nucleotides", dNTPs)
+CodonSort, z3codons = EnumSort("Codons", triplet_dna_codons)
+AminoSort, z3aminos = EnumSort("Amino Acids", aminoacids)
 
 # z3amino definitions
 STOP, NULL = z3aminos[-2:]
@@ -49,5 +52,16 @@ amino_to_z3amino = {
 }
 
 # z3 Functions
-f_Codon = Function("nucleotides -> codons", N, N, N, C)     # F(N,N,N) -> C
-f_Code = Function("Genetic Code", C, A)                     # F(C) -> A
+f_nuc_to_codon = Function("nucleotides -> codons",
+                          NucleotideSort, NucleotideSort, NucleotideSort,
+                          CodonSort)
+f_codon_to_amino = Function("codons -> aminos", CodonSort, AminoSort)
+f_nuc_to_amino = Function("nucleotides -> aminos",
+                          NucleotideSort, NucleotideSort, NucleotideSort,
+                          AminoSort)
+
+
+# z3 Constant generating functions
+def sequence_variables(loc: Location):
+    return [Const(f"x{i}", NucleotideSort)
+            for i in range(loc.start, loc.end)]
